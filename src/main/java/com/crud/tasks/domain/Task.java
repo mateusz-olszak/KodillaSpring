c8f2aae0a797b0702aaebb6bea9f55e5f274fd1a
Mateusz Olszak <mateuszolszak98@gmail.com>
2021-09-02 12:35:12 +0200

    New Repository

---
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -0,0 +1,16 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Task {
+
+    private Long id;
+    private String title;
+    private String content;
+
+}
